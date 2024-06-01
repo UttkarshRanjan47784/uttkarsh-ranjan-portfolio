dcd7a632a423c4126d283efc66961e8cd15408ab
UttkarshRanjan47784 <utr.1206@gmail.com>
2024-06-01 12:26:27 +0530

    deploy test for new font color. Trimmed Intro.

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,16 +5,11 @@ export default function HeroSection() {
     <div className="grow flex flex-col justify-center mx-10 md:mx-24 text-left">
       <p className="text-sm font-mono md:text-xl ml-1 ">Hi! My name is</p>
       <h1 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl">
-        Uttkarsh Ranjan
+        U<span className="text-red-600">tt</span>karsh Ranjan
       </h1>
-      <h3 className="mt-3 w-5/6 text-sm md:text-base md:w-4/5 md:text-red-400 lg:text-yellow-400 lg:w-3/5 ml-1">
+      <h3 className="mt-3 w-5/6 text-sm md:text-base md:w-4/5 lg:w-3/5 ml-1">
         I am a passionate Full Stack Web Developer with a knack for creating web
-        apps with a clean UI and a robust backend.{" "}
-        <span className="hidden sm:inline">
-          With a strong foundation in both front-end and back-end technologies,
-          I excel at transforming ideas into seamless digital experiences. I
-          believe in creating clean and simple UI and robust backends.
-        </span>
+        apps with a clean UI and a robust backend.
       </h3>
       <div className="flex space-x-3 mt-5">
         <Button className="transition-all hover:scale-105">View Resume</Button>
